9d3a888b685e37f3fdbc71ff297d25601e374b7d
Jean-Baptiste MERAND <jbmerand@isagri.fr>
2024-09-17 16:58:05 +0200

    Add Vuetify

--- vue-apollo-sandbox/src/main.ts
+++ vue-apollo-sandbox/src/main.ts
@@ -1,14 +1,30 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-const app = createApp(App)
+// Components
+import App from './App.vue';
+import router from './router';
 
-app.use(createPinia())
-app.use(router)
+const vuetify = createVuetify({
+    theme: {
+        defaultTheme: 'dark'
+    },
+    components,
+    directives,
+});
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(vuetify);
+app.use(router);
+
+app.mount('#app');
